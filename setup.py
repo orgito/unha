60605eb263d25a7728dc85652e162b1d5179cbc4
Renato Orgito <orgito@gmail.com>
2018-07-05 16:45:14 -0300

    Change python support to 3.4+

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='unha',
-    version='0.0.6',
+    version='0.0.7',
     description='A library to simplify telnet connections to Cisco IOS devices',
     long_description=open('README.rst', 'r').read(),
     author='Renato Orgito',
@@ -16,12 +16,15 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='unha network telnet automation',
     packages=['unha'],
     install_requires=[],
-    python_requires='>=3.6',
+    python_requires='>=3.4',
     project_urls={
         'Bug Reports': 'https://github.com/orgito/unha/issues',
         'Source': 'https://github.com/orgito/unha',
